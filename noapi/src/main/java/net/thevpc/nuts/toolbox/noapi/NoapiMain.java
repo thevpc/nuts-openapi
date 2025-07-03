1115bd2d88777c38822950662f3186756d485daa
thevpc <taha.bensalah@gmail.com>
2025-07-03 19:00:25 +0100

    [FIXED] replace NApplication.main(MyApp.class, args); by NApp.builder(args).run(); which is more elegant

--- noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/NoapiMain.java
+++ noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/NoapiMain.java
@@ -22,7 +22,7 @@ public class NoapiMain implements NApplication {
     private List<NoapiCmdData> data = new ArrayList<>();
 
     public static void main(String[] args) {
-        new NoapiMain().main(NMainArgs.ofHandled(args));
+        NApp.builder(args).run();
     }
 
     @Override
