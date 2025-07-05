9082118049af62a0a93655260b4132641542cea3
thevpc <taha.bensalah@gmail.com>
2025-07-05 21:43:26 +0100

    [CHANGED] changed NApp.Definition to NAppDefinition

--- noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/NoapiMain.java
+++ noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/NoapiMain.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@NApp.Definition
+@NAppDefinition
 public class NoapiMain  {
 
     private NOpenAPIService service;
@@ -26,7 +26,7 @@ public class NoapiMain  {
         NApp.builder(args).run();
     }
 
-    @NApp.Runner
+    @NAppRunner
     public void run() {
         NSession session = NSession.of();
         this.service = new NOpenAPIService(session);
