be46239e476c2d9a936b6cf9685edea6a54b10b0
thevpc <taha.bensalah@gmail.com>
2025-08-05 10:01:11 +0100

    Update noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/store/NoApiStore.java

--- noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/store/NoApiStore.java
+++ noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/store/NoApiStore.java
@@ -8,37 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface NoApiStore {
-    NOptional<String> getTitle();
-
-    NOptional<String> getVersion();
-
-    List<DocItemInfo> getMultiDocuments();
-
-    String getId();
-
-    List<MVar> findVariables();
-
-    List<MVar> findConfigVariables();
-
-    NOptional<String> getDescription();
-
-    NOptional<String> getConfigDescription();
-
-    NOptional<MContact> getContact();
-
-    List<MChangeLog> findChangeLogs();
-
-    List<MHeader> findHeaders();
-
-    List<MSecurityScheme> findSecuritySchemes();
-
-    List<MServer> findServers();
-
-    Map<String, TypeInfo> findTypesMap();
-
-    List<MPath> findPaths();
-
-    List<TypeCrossRef> typeCrossRefs();
+    MStoreModel loadStoreModel(NPath cf);
 
     MConf loadConfigFile(NPath cf);
 
