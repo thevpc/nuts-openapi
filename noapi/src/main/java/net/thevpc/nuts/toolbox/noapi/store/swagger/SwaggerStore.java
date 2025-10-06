7439aafdd44727371f87c48dc77f87a81500fc7b
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:25:50 +0100

    Update SwaggerStore.java : bump nuts 0.8.7

--- noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/store/swagger/SwaggerStore.java
+++ noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/store/swagger/SwaggerStore.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.toolbox.noapi.store.swagger;
 
-import net.thevpc.nuts.NIllegalArgumentException;
+import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.elem.*;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.toolbox.noapi.model.*;
