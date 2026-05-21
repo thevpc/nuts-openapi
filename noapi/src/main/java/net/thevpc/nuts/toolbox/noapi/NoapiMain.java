044ccba4cb9f705b4985096268b59f0abfbb664a
thevpc <taha.bensalah@gmail.com>
2026-05-21 15:52:12 +0100

    [REFACTOR] uniform naming (WIP)

--- noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/NoapiMain.java
+++ noapi/src/main/java/net/thevpc/nuts/toolbox/noapi/NoapiMain.java
@@ -90,9 +90,9 @@ public class NoapiMain  {
                                 String vars = a.getStringValue().get();
                                 NArg b = NArg.of(vars);
                                 if (b.isUncommented()) {
-                                    ref.getVarsMap().put(b.getKey().toStringLiteral(), b.toLiteral().toStringLiteral());
+                                    ref.getVarsMap().put(b.getKey().toStringLiteral(), b.literalValue().toStringLiteral());
                                     if (!data.isEmpty()) {
-                                        data.get(data.size() - 1).getVarsMap().put(b.getKey().toStringLiteral(), b.toLiteral().toStringLiteral());
+                                        data.get(data.size() - 1).getVarsMap().put(b.getKey().toStringLiteral(), b.literalValue().toStringLiteral());
                                     }
                                 }
                             }
